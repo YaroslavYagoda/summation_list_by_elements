bf1027ba52b67bd97f017879aa6028bd0818bb9e
Ярослав <Yagavdv14@yandex.ru>
2024-09-11 20:07:21 +0300

    Исправлено в двух вариантах

--- main.py
+++ main.py
@@ -3,11 +3,26 @@
 В новом списке каждый элемент является суммой соответствующих элементов входных списков.
 Программа должна делать это на основе циклов и работать со списками любой длины.
 """
-from random import randint
-from itertools import zip_longest
+from random import randint as ri
+from itertools import zip_longest as zl
 
-sp2 = [_ for _ in range(randint(0, 5_000))]
-sp1 = [_ for _ in range(randint(0, 5_000))]
+# Решение с использованием знаний курса
+sp2 = [i for i in range(3)]
+sp1 = [i for i in range(2)]
+sp3 = []
 
-sp3 = [sum(i) for i in zip_longest(sp1, sp2, fillvalue=0)]
+if len(sp1) > len(sp2):
+    for i in range(len(sp2)):
+        sp3.append(sp1[i] + sp2[i])
+    for i in range(len(sp2), len(sp1)):
+        sp3.append(sp1[i])
+else:
+    for i in range(len(sp1)):
+        sp3.append(sp1[i] + sp2[i])
+    for i in range(len(sp1), len(sp2)):
+        sp3.append(sp2[i])
+
 print(sp3)
+
+# Решение в одну строку
+print([sum(i) for i in zl([i for i in range(ri(0, 50))], [i for i in range(ri(0, 50))], fillvalue=0)])
