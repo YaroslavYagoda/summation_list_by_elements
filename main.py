060e76db39638e13e9c99ba45b1498079822f3f1
Ярослав <Yagavdv14@yandex.ru>
2024-09-11 13:23:10 +0300

    Ready2

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 В новом списке каждый элемент является суммой соответствующих элементов входных списков.
 Программа должна делать это на основе циклов и работать со списками любой длины.
 """
-import random
+from random import randint
 from itertools import zip_longest
 
-sp2 = [_ for _ in range(random.randint(0, 5_000))]
-sp1 = [_ for _ in range(random.randint(0, 5_000))]
+sp2 = [_ for _ in range(randint(0, 5_000))]
+sp1 = [_ for _ in range(randint(0, 5_000))]
 
 sp3 = [sum(i) for i in zip_longest(sp1, sp2, fillvalue=0)]
 print(sp3)
